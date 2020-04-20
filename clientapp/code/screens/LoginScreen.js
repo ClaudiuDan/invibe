916a8b881e1d3048b5cfc90881f62eebcc77782d
Claudiu Dan <claudiudanionut@yahoo.com>
2020-04-20 19:07:50 +0300

    remove console.log

--- clientapp/code/screens/LoginScreen.js
+++ clientapp/code/screens/LoginScreen.js
@@ -57,7 +57,6 @@ class LoginScreen extends Component {
         permissions: ['public_profile', 'email'],
       });
       if (type === 'success') {
-        console.log(token);
         // Get the user's name using Facebook's Graph API
         const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
         this.setState({token:token});
