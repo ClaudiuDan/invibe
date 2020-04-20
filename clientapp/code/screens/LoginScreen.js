b0c4d4fa41b7e8459f11f8f8cd5e2220d9428a12
Claudiu Dan <claudiudanionut@yahoo.com>
2020-04-20 19:06:46 +0300

    fix frontend social login bug

--- clientapp/code/screens/LoginScreen.js
+++ clientapp/code/screens/LoginScreen.js
@@ -60,6 +60,7 @@ class LoginScreen extends Component {
         console.log(token);
         // Get the user's name using Facebook's Graph API
         const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+        this.setState({token:token});
         this.handleSocialRequest();
         Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
       } else {
